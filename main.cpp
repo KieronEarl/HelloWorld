c54ac8ff5d809f776132b3cf836b1f26fe6ec822
Kieron Earl <ke69@brighton.ac.uk>
2012-10-15 11:32:32 +0100

    updated into basic input/output program

--- main.cpp
+++ main.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main()
-{
-
-	cout << "Hello, world!" << endl;
-	return 0;
+void printOut(string toPrint){
+	cout << toPrint << endl;
 
 }
+
+void printOut(string toPrint, string toPrint2){
+	cout << toPrint << toPrint2 << endl;
+}
+
+string readIn(){
+	string result;
+	cin >> result;
+	return result;
+}
+
+int main()
+{
+	printOut("Enter in a value");
+	string value = readIn();
+	printOut("You entered: ", value);
+	return 0;
+}
+
+
