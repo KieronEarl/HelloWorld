0942f0f29e2d4378dfceedda24b07b69bdca0f82
Kieron Earl <ke69@brighton.ac.uk>
2012-10-15 11:45:45 +0100

    More complicated

--- main.cpp
+++ main.cpp
@@ -12,18 +12,33 @@ void printOut(string toPrint, string toPrint2){
 	cout << toPrint << toPrint2 << endl;
 }
 
+void printOut(string toPrint, char toPrint2){
+	cout << toPrint << toPrint2 << endl;
+}
+
 string readIn(){
 	string result;
 	cin >> result;
 	return result;
 }
 
+void facing(){
+	string facing;
+	printOut("Turn to face which direction?");
+	string value = readIn();
+	if(value=="N"||value=="E"||value=="S"||value=="W"){
+		facing = value;
+		printOut("You are now facing: ", facing);
+	}
+	else{
+		printOut("Unknown command");
+		printOut("You are still facing", facing);
+	}
+}
+
 int main()
 {
-	printOut("Enter in a value");
-	string value = readIn();
-	printOut("You entered: ", value);
-	return 0;
+	facing();
 }
 
 
